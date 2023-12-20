d77b1e35d45d156c405d94a9249483f01270513a
Mbonyimfura <mbonyimfuratresor@gmail.com>
2023-12-20 21:29:24 +0200

    Add user routes

---
+++ src/routes/user.js
@@ -0,0 +1,21 @@
+const displayHome = (req,res) =>{
+
+}
+
+const getUsers  = () =>{
+
+}
+const getUserById  = () =>{
+    
+}
+const createUser  = () =>{
+    
+}
+
+const updateUser = () =>{
+    
+}
+
+module.exports = {
+    displayHome, getUsers, getUserById, createUser, updateUser
+}
